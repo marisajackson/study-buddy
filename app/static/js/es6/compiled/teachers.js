79ad95674389bbecae8cd17710223e286eaf54c5
asejdina <asejdina@gmail.com>
2014-05-23 09:29:47 -0500

    created jade and css files

--- app/static/js/es6/compiled/teachers.js
+++ app/static/js/es6/compiled/teachers.js
@@ -1,2 +1,19 @@
+(function() {
+  'use strict';
+  $(document).ready(init);
+  function init() {
+    $('#createCourse').on('click', createCourse);
+    $('#add').on('click', addQuestion);
+  }
+  function addQuestion() {
+    var question = $('form#course > .questionField:last-child');
+    $('form#course').append(question.clone());
+  }
+  function createCourse() {
+    ajax("/courses/new", 'GET', null, (function(html) {
+      $('#courses').append(html);
+    }));
+  }
+})();
 
 //# sourceMappingURL=teachers.map
