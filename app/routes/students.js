1c185c77342efcf087281eac926bcb013ac9af4d
MaRisa Jackson <mhljackson@gmail.com>
2014-05-23 17:29:05 -0500

    course mvp

--- app/routes/students.js
+++ app/routes/students.js
@@ -4,7 +4,7 @@ var traceur = require('traceur');
 var Course = traceur.require(__dirname + '/../models/course.js');
 
 exports.index = (req, res)=>{
-  if(req.session.userId || req.session.userType !=='student') {
+  if(!req.session.userId || req.session.userType !=='student') {
     res.redirect('/');
   }
   Course.findAll(courses=>{
